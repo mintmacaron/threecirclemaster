fd676ec706d5cda981e427d51c323db04bd8166e
mintmacaron <48581091+mintmacaron@users.noreply.github.com>
2019-07-29 02:32:17 +0900

    first commit

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SCRET_KEY', 5460)
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 5460)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool (os.environ.get('DJANGO_DBUG', True))
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,3 +136,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 # '홈페이지 이름/media/file이름' url 설계
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
